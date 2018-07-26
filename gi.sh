0b9feaa19275b195747f492b2e42fe9cb87110b8
Nerdmaster <brody76@gmail.com>
2018-07-25 20:38:52 -0700

    Cleanup gi.sh

--- gi.sh
+++ gi.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -eu
 
-for file in $(find . -name "*.go"); do echo ${file%/*}; done | sed "s|\.|github.com/Nerdmaster/sendmail|g" | sort | uniq | xargs -l1 go install
+for file in $(find . -name "*.go"); do
+  echo ${file%/*}
+done | \
+  sed "s|\.|github.com/Nerdmaster/sendmail|g" | \
+  sort | uniq | \
+  xargs -l1 go install
